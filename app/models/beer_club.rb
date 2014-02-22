9ee846a1f565287c2f17f60499a031e015d26463
mihassin <marko.hassinen@helsinki.fi>
2014-02-22 20:02:44 +0200

    vk6

--- app/models/beer_club.rb
+++ app/models/beer_club.rb
@@ -1,9 +1,17 @@
 class BeerClub < ActiveRecord::Base
+  def future_year
+    if year > Date.today.year
+      errors.add(:year, "seems to be incorrect")
+    end
+  end
+
   has_many :memberships, dependent: :destroy
   has_many :users, through: :memberships
 
   validates_presence_of :name, :founded, :city
   validates :founded, numericality: { greater_than_or_equal_to: 1800, only_integer: true }
+  validate :future_year
+  validates :name, uniqueness: true, length: { minimum: 3, maximum: 30 }
 
   def member?(user)
     users.include? user and memberships.find_by(user_id:user.id).confirmed
--- app/models/style.rb
+++ app/models/style.rb
@@ -4,6 +4,8 @@ class Style < ActiveRecord::Base
   has_many :beers
   has_many :ratings, through: :beers
 
+  validates :name, length: { minimum: 2 }
+
   def self.top(n)
     sorted_by_rating_in_desc_order = Style.all.sort_by{ |b| -(b.average_rating||0) }
     sorted_by_rating_in_desc_order.take(n)
